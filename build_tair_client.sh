5f300f287e4d15bde27236ef1a54f12e2da35911
Zhiyi Sun <zhiyisun@msn.com>
2016-10-22 19:00:39 +0800

    Update script.
    Signed-off-by: Zhiyi Sun <zhiyisun@msn.com>

---
+++ build_tair_client.sh
@@ -0,0 +1,26 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: build_tair_client.sh
+# 
+#         USAGE: ./build_tair_client.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Zhiyi Sun (zsun), zsun@apm.com
+#  ORGANIZATION: 
+#       CREATED: 10/22/2016 06:39:03 PM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+BASE_DIR=${HOME}/workspace/software/demo/taobao
+TAIR_JAVA_CLIENT=tair-java-client
+cd ${BASE_DIR}/${TAIR_JAVA_CLIENT}
+mvn clean package
+cp target/tair-client-2.3.5.jar ~/.m2/repository/com/taobao/tair/tair-client/2.3.5/
